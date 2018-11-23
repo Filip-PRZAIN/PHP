5ee7e28da37c2a85cb25fd6fbd984c5a8071864b
Filip-PRZAIN <45295631+Filip-PRZAIN@users.noreply.github.com>
2018-11-23 18:34:05 +0100

    Add files via upload

---
+++ RegEx/PHP-1.php
@@ -0,0 +1,4 @@
+<?php
+$password=$_GET['password'];
+
+echo "You've been hacked and they know your password<br><span style='color:red; font-size:36px; text-decoration-style:solid'>$password</span>";
---
+++ RegEx/PHP-2.php
@@ -0,0 +1,6 @@
+<?php
+$text=$_POST['text'];
+
+echo "<span style='align-content:center; background-color:beige; font-family:Comic Sans MS, cursive, sans-serif'>WHAT NOW?</span><br>";
+sleep(3);
+echo "WE DON'T KNOW!";
